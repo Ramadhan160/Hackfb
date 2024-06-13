07f616fb32f7c91d25b552f62e17d9d16c11c281
Putra Maulana <110748674+Ramadhan160@users.noreply.github.com>
2024-06-14 06:15:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,22 +22,37 @@ echo
 gem install lolcat
 clear
 echo
-echo "           .^!?JYYYJ?!^." | lolcat           
-echo "        .7G#@@@@@@@@@@@#G7." | lolcat          
-echo "       ^#@@@@@@@@@@@@@@@@@#^" | lolcat         
-echo "      .BG&@@@@@@@@@@@@@@@@GB." | lolcat        
-echo "      :&^&@@@@@@@@@@@@@@@@~#^" | lolcat        
-echo "       GYY@@&&&@@@@@&&&@@5?B." | lolcat        
-echo "       :7?P^::.~@@@! ::^YY!:" | lolcat         
-echo "   :     G5   .5@@@P.   Y#     :." | lolcat   
-echo "  7@J    B@BYY#&7J!&&YYB@#.   J@?" | lolcat    
-echo " 7B##BP?^^75B@@Y J J@@B5?^~?PB#BB?" | lolcat   
-echo " .. .^75GBJ^7?&#G&GB@?7~?BG57^. .." | lolcat   
-echo "         :^~5?J55555J?5!^:" | lolcat           
-echo "         ^!~#5YJJJJJY5#!7^." | lolcat          
-echo "   !Y?JPBBP^~B&&&&&&&B!:5BBGJ?Y!" | lolcat     
-echo "   .Y@&?^     :~!!!~:     :7#@5:" | lolcat     
-echo "    .J:                     .J:" | lolcat      
+echo "                              ..::::::.." | lolcat                              
+echo "                      .^!?YPGB##&&&&&&&#BGPY?!^." | lolcat                      
+echo "                   :?P#@@@@@@@@@@@@@@@@@@@@@@@@#G?:" | lolcat                   
+echo "                ~P&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@P~" | lolcat                 
+echo "               .5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5." | lolcat               
+echo "              .G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B." | lolcat              
+echo "              P@@#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#&@P" | lolcat              
+echo "             ^@@!:&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!:&@^" | lolcat             
+echo "             ~@@.^@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@7 B@!" | lolcat             
+echo "             .&@! B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&::&@~" | lolcat             
+echo "              Y@#.~@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@? G@B" | lolcat              
+echo "              .G@? B@@&#BGPP5YP&@@@@@@@GY55PGB#&@@&.~@#^" | lolcat              
+echo "               .G? B&?:.       ~@@@@@@?        .!#@:^P:" | lolcat               
+echo "                  :@P          ^@@@@@@7          ?@7" | lolcat                  
+echo "                  J@@!        !&@@@@@@@?        ^#@G" | lolcat                  
+echo "     !P?.         B@@&7.    :5@@@BBBB@@@G~     !#@@@:        .JB7" | lolcat     
+echo "     G@@G:        G@@@@#PYYP&@@@? ?Y 7&@@@GYY5B@@@@#.       :B@@B" | lolcat     
+echo "   .?&@@@&J~.     ^B@@@@@@@@@@@7  JY  !@@@@@@@@@@@#!     .~J#@@@@Y:" | lolcat   
+echo "  ?&@@@@@@@@&GJ!:   :7J5B@@@@@&.  ?Y   #@@@@@#PJ7^   :!YG&@@@@&@@@&J" | lolcat  
+echo "  :~~~^:^~JP#@@@@B5?^ ~::^J&@@@7.!B#7.~&@@@5~::! ^75B@@@@#GJ!^:^~~~:" | lolcat  
+echo "            .^7YB&@@@7.Y7G ~@@@@&@@@@&@@@@? Y!5:!@@@&B57^." | lolcat            
+echo "                 .~?PG 7J55:55GPGGGGGGPP55~J5JJ 5P?~:" | lolcat                 
+echo "                     . ?&~B7YJP?YY5J5JJ5YJ?G~#J" | lolcat                       
+echo "                       J@?77Y?Y7JJJ?Y??JJJJ!?@Y" | lolcat                       
+echo "                  .^75:^@@GY?JJ?JYY?5?JJJ?YG@@~.5?~." | lolcat                  
+echo "              :~JP#@@@P !&@@@&BBGGPPGGBB&@@@&7 Y@@@&GY7^." | lolcat             
+echo "      !B#BPPGB&@@@&GJ!:  :Y#@@@@@@@@@@@@@@#Y:  :~?P#@@@@#GPPB#B?" | lolcat      
+echo "       !P@@@@@&GJ~.        .~7J5PGGGGP5J7^.        .^?P#@@@@@G7." | lolcat      
+echo "         J@@@5:                                        .J@@@P" | lolcat         
+echo "         ^&#7                                            !B@7" | lolcat         
+echo "          ^.                                              .^" | lolcat
 echo
 echo
 clear
