d5ae48a1bd3b6e8f1f4007be3c12876cbd50b5ea
Putra Maulana <110748674+Ramadhan160@users.noreply.github.com>
2024-06-14 05:29:01 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,22 +11,22 @@ echo "       ░█▄▄█ ░█─░█ ░█▄▄█ ░█─░█ ▄
 echo
 apt install ruby -y
 echo
-echo "                   .:^^~77~'   
-echo "                .?PB#&&&###5'  
-echo "                G##&&&&&&&BB!' 
-echo "               7&&&&&&&&&&BB^' 
-echo "              !P&&&&&&&&##BJ'  
-echo "             .PP#@@@@&&##BP.'  
-echo "             YPGB&@@@&&#BBY'   
-echo "            7GBB&&&&&&#BG7'    
-echo "           ~PGBB##BBGGP5^'     
-echo "          ~YGBBBBGP55Y7.'      
-echo "    .   :7YGGGGGGP5YJ^'        
-echo "...::.^7Y5PGBPP55JJ!.'         
-echo "..^^^7YPGGBBBG5YJ7:'           
-echo ".:~!JPPGBBBGP5J7^'             
-echo ":^~7Y55PP5YJ7~.  .:^.'         
-echo "^:^^:^^^^:.........:~~'
+echo "                       .:^^~77~"   
+echo "                    .?PB#&&&###5"  
+echo "                    G##&&&&&&&BB!" 
+echo "                   7&&&&&&&&&&BB^" 
+echo "                  !P&&&&&&&&##BJ"  
+echo "                 .PP#@@@@&&##BP."  
+echo "                 YPGB&@@@&&#BBY"   
+echo "                7GBB&&&&&&#BG7"    
+echo "               ~PGBB##BBGGP5^"    
+echo "              ~YGBBBBGP55Y7."      
+echo "        .   :7YGGGGGGP5YJ^"        
+echo "    ...::.^7Y5PGBPP55JJ!."         
+echo "    ..^^^7YPGGBBBG5YJ7:"           
+echo "    .:~!JPPGBBBGP5J7^"             
+echo "    :^~7Y55PP5YJ7~.  .:^."         
+echo "    ^:^^:^^^^:.........:~~"
 echo 
 gem install lolcat
 clear
