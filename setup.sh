c4d474b10eb1a2a395d6f599883858d42b2e0c11
Putra Maulana <110748674+Ramadhan160@users.noreply.github.com>
2024-06-14 06:00:08 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,12 +32,22 @@ echo
 gem install lolcat
 clear
 echo
-echo "          ╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮"╭╮╮╱▔▔▔▔╲╭╭╮" | lolcat
-echo "         ╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯" | lolcat
-echo "         ┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈" | lolcat
-echo "         ┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈" | lolcat
-echo "         ╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮" | lolcat
-echo "         ╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯" | lolcat
+echo "           .^!?JYYYJ?!^." | lolcat           
+echo "        .7G#@@@@@@@@@@@#G7." | lolcat          
+echo "       ^#@@@@@@@@@@@@@@@@@#^" | lolcat         
+echo "      .BG&@@@@@@@@@@@@@@@@GB." | lolcat        
+echo "      :&^&@@@@@@@@@@@@@@@@~#^" | lolcat        
+echo "       GYY@@&&&@@@@@&&&@@5?B." | lolcat        
+echo "       :7?P^::.~@@@! ::^YY!:" | lolcat         
+echo "   :     G5   .5@@@P.   Y#     :." | lolcat   
+echo "  7@J    B@BYY#&7J!&&YYB@#.   J@?" | lolcat    
+echo " 7B##BP?^^75B@@Y J J@@B5?^~?PB#BB?" | lolcat   
+echo " .. .^75GBJ^7?&#G&GB@?7~?BG57^. .." | lolcat   
+echo "         :^~5?J55555J?5!^:" | lolcat           
+echo "         ^!~#5YJJJJJY5#!7^." | lolcat          
+echo "   !Y?JPBBP^~B&&&&&&&B!:5BBGJ?Y!" | lolcat     
+echo "   .Y@&?^     :~!!!~:     :7#@5:" | lolcat     
+echo "    .J:                     .J:" | lolcat      
 echo
 echo
 clear
