ece9ef827b493c2c6cfc259ac57164a3d24394be
Putra Maulana <110748674+Ramadhan160@users.noreply.github.com>
2024-06-14 09:39:15 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,16 +22,16 @@ echo
 gem install lolcat
 clear
 echo      
-echo "                  .-"      "-." | lolcat
-echo "                 /            \" | lolcat
+echo "                  .-"      "-.  " | lolcat
+echo "                 /            \ " | lolcat
 echo "                |              |" | lolcat
 echo "                |,  .-.  .-.  ,|" | lolcat
 echo "           /\   | )(__/  \__)( |" | lolcat
 echo "         _ \/   |/     /\     \|" | lolcat
-echo "        \_\/    (_     ^^     _)   .-==/~\" | lolcat
+echo "        \_\/    (_     ^^     _)   .-==/~\ " | lolcat
 echo "       ___/_,__,_\__|IIIIII|__/__)/   /{~}}" | lolcat
 echo "       ---,---,---|-\IIIIII/-|---,\'-' {{~}" | lolcat
-echo "                  \          /     '-==\}/" | lolcat
+echo "                  \          /     '-==\}/ " | lolcat
 echo "                   `--------`" | lolcat
 echo                                                                                                                                                                                    
 echo
